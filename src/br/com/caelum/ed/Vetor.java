51000ca97737a54f90076a7956bb4b3d9c0be6e1
DESKTOP-P2Q8L7H\Lucas <lucas.magalhaes911@gmail.com>
2022-07-04 22:24:06 -0300

    adicionando o método garanta espaço

--- src/br/com/caelum/ed/Vetor.java
+++ src/br/com/caelum/ed/Vetor.java
@@ -8,11 +8,13 @@ public class Vetor {
     private int totalDeAlunos = 0;
 
     public void adiciona(Aluno aluno){
+            this.garantaEspaco();
             this.alunos[this.totalDeAlunos] = aluno;
             this.totalDeAlunos++;
     }
 
     public void adiciona(int posicao, Aluno aluno){
+        this.garantaEspaco();
 
         if (!this.posicaoOcupada(posicao)){
             throw new IllegalArgumentException("Posicao inválida");
@@ -71,6 +73,16 @@ public class Vetor {
 
     private boolean posicaoValida(int posicao){
         return posicao >= 0 && posicao <=this.totalDeAlunos;
+    }
+
+    private void garantaEspaco(){
+        if (this.totalDeAlunos == this.alunos.length){
+            Aluno[] novaArray = new Aluno[this.alunos.length * 2];
+            for (int i = 0; i < this.alunos.length; i++) {
+                novaArray[i] = this.alunos[i];
+            }
+            this.alunos = novaArray;
+        }
     }
 
     @Override
